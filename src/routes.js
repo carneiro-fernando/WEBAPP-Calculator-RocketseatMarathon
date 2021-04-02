2f3e27cd604a667e40ff8a214fa3701dd40f24db
carneiro-fernando <carneiro.fernando@icloud.com>
2021-04-02 17:42:11 -0300

    2th class (3:32)

--- src/routes.js
+++ src/routes.js
@@ -1,58 +1,138 @@
+const { json } = require("express");
 const express = require("express");
 const routes = express.Router()
 const views = __dirname + "/views/"
 
-//constantes
-var createdAt = Date.now()
+const Profile = {
+    data: {
+        name: "Fernando Carneiro    ",
+        avatar: "https://github.com/carneiro-fernando.png",
+        "monthly-budget": 3000,
+        "days-per-week": 3,
+        "hours-per-day": 8,
+        "vacation-per-year": 4,
+        "hours-value": 75
+    },
 
-const profile = {
-    name: "Fernando Carneiro    ",
-    avatar: "https://github.com/carneiro-fernando.png",
-    "monthly-budget": 3000,
-    "days-per-week": 3,
-    "hours-per-day": 8,
-    "vacation-per-year": 4
+    controllers: {
+        index(req, res) {
+            return res.render(views + "profile", { profile: Profile.data })
+        },
+
+        update(req,res) {
+            //req.body para pegar os dados
+            const data = req.body
+            //Semanas no ano
+            const weeksPerYear = 52
+            //remover as férias do ano e pegar média de semanas no mês
+            const weeksPerMonth = (weeksPerYear - data["vacation-per-year"]) / 12
+            //total de horas trabalhadas na semana
+            weekWorkedHours = data["hours-per-day"] * data["days-per-week"]
+            //horas trabalhadas no mês
+            const monthlyWorkedHours = weekWorkedHours * weeksPerMonth
+            //valor da hora 
+            const valueHour = data["monthly-budget"] / monthlyWorkedHours
+
+            Profile.data = {
+                ...Profile.data,
+                ...req.body,
+                avatar: Profile.data.avatar,
+                "hours-value": valueHour
+            }
+            return res.redirect('/profile')
+        }
+    }
 }
 
-//constante que recebe array de jobs
-const jobs = [
-    {
-        name: "Pizzaria idiota",
-        "daily-hours": 2,
-        "total-hours": 60,
-        createdAt:Date.now()
+
+const Job = {
+    data: [
+        {
+            id: 1,
+            name: "Pizzaria ",
+            "daily-hours": 2,
+            "total-hours": 2,
+            createdAt: Date.now()
+        },
+        {
+            id: 2,
+            name: "Project ",
+            "daily-hours": 2,
+            "total-hours": 10,
+            createdAt: Date.now()
+        }
+    ],
+
+    controllers: {
+        index(req, res) {
+            //Criação de array novo que adiciona campos ao array jobs
+            const updatedJobs = Job.data.map((job) => {
+
+                const remaining = Job.services.remainingDays(job);
+                const newStatus = remaining <= 0 ? true : false;
+                const status = newStatus;
+
+                return {
+                    ...job, //spread da array job 
+                    remaining, //dias restantes
+                    status, //status da job
+                    budget: Profile.data["hours-value"] * job["total-hours"] //valor da hora
+                }
+            })
+            return res.render(views + "index", { jobs: updatedJobs })
+        },
+
+        create(req, res) {
+            return res.render(views + "job")
+        },
+
+        save(req, res) {
+
+            //Criando identificardor do job
+            const jobLast = Job.data.length - 1?.id || 1 //constante do tamanho do array job
+
+            //empurrando dados da requisição para a constante. (Jobs.push(req.body + createdAt) - A maneira que pensei em fazer)
+            Job.data.push({
+                id: jobLast + 1,
+                name: req.body.name,
+                "daily-hours": req.body["daily-hours"],
+                "total-hours": req.body["total-hours"],
+                createdAt: Date.now()
+            })
+
+            //retornando os dados para a página inicial
+            return res.redirect('/')
+
+        }
     },
-    {
-        name: "Project otario",
-        "daily-hours": 4,
-        "total-hours": 47,
-        createdAt:Date.now()
+
+    services: {
+        remainingDays(job) {
+
+            const createdDate = new Date(job.createdAt)
+            const termlength = (job['total-hours'] / job['daily-hours']).toFixed()
+            const deadline = createdDate.setDate(createdDate.getDate() + Number(termlength))
+            const remainingDays = Math.floor((deadline - Date.now()) / (8.64 * Math.pow(10, 7)))
+
+            return remainingDays
+        }
     }
-]
+}
+
+// Constantes
+const createdAt = Date.now() //constante que define a data atual
+
+
 
 //Rotas GET
-routes.get('/', (req, res) => res.render(views + "index", {jobs}))
-routes.get('/job', (req, res) => res.render(views + "job"))
+routes.get('/', Job.controllers.index)
+routes.get('/job', Job.controllers.create)
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
-routes.get('/profile', (req, res) => res.render(views + "profile", { profile }))
+routes.get('/profile', Profile.controllers.index)
+routes.post('/profile', Profile.controllers.update)
 
 //Rotas POST
-routes.post('/job', (req, res) => {
-
-    const jobId = + jobs.length
-
-    //empurrando dados da requisição para a constante. (Jobs.push(req.body + createdAt) - A maneira que pensei em fazer)
-    jobs.push({
-        name: req.body.name,
-        "daily-hours": req.body["daily-hours"],
-        "total-hours": req.body["total-hours"],
-        createdAt:Date.now()
-    })
-    console.log(createdAt)
-    //retornando os dados para a página inicial
-    return res.redirect('/')
-
-})
+routes.post('/job', Job.controllers.save)
 
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+const { urlencoded } = require("express")
 const express = require("express")
 const server = express()
 const routes = require("./routes")
@@ -8,7 +9,11 @@ server.set("view engine", "ejs")
 //Enable static assets
 server.use(express.static("public"))
 
+//Usar o req.body
+server.use(express.urlencoded({ extended: true}))
+
 //Routes
 server.use(routes)
 
+//configuração da porta
 server.listen(3000, () => console.log(""))
